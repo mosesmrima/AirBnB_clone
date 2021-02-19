ec9141c716162cad5f6b1cf8ff33250c579e85b2
mosesmrima <mrimamss@gmail.com>
2021-02-19 23:14:30 +0300

    console 0.0.1

--- console.py
+++ console.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 import cmd
-from models.base_model import BaseModel
-from models import storage
 """ module contains the console implementation"""
 
 
-def parse(arg):
-    """parses commandline arguments"""
-    args = arg.split(" ")
-    return args
-
-
 class HBNBCommand(cmd.Cmd):
+    """console for HBnB"""
     prompt = "(hbnb)"
-    __classes = {
-        "BaseModel",
-    }
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -25,36 +15,6 @@ class HBNBCommand(cmd.Cmd):
         """exit console"""
         print("")
         return True
-
-    def emptyline(self) -> bool:
-        pass
-
-    def do_create(self, arg):
-        """creates a new class instance"""
-        args = parse
-        if len(arg) == 0:
-            print("** class name missing **")
-        elif arg not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        else:
-            obj = BaseModel()
-            print(obj.id)
-            storage.save()
-
-    def do_show(self, arg):
-        """prints all the instances"""
-        args = parse(arg)
-        objdict = storage.all()
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
-        elif "{}.{}".format(args[0], args[1]) not in objdict:
-            print("** no instance found **")
-        else:
-            print(objdict["{}.{}".format(args[0], args[1])])
 
 
 if __name__ == '__main__':
