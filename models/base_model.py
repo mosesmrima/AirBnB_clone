e6d16f3b161d81295c261f50dd0d0148e1e94dc8
mosesmrima <mrimamss@gmail.com>
2021-02-19 22:36:00 +0300

    base

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module contains one class, BaseModel"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Represents the base model class for the HBnB console"""
+    def __init__(self):
+        """__init__ method called by every new instance"""
+        self.id = uuid4()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        cls_name = self.__class__.__name__
+        string = "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
+        return string
+
+    def save(self):
+        """Updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary of the BaseModel Instance
+        :return: dictionary containing key/value pairs of an object
+        """
+        mydict = self.__dict__.copy()
+        mydict["created_at"] = self.created_at.isoformat()
+        mydict["updated_at"] = self.updated_at.isoformat()
+        mydict["__class__"] = self.__class__.__name__
+        return mydict
