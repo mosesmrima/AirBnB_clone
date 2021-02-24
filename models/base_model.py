2c6a5a143d7912c522aad3216fb2ee42b69c59b0
mosesmrima <mrimamss@gmail.com>
2021-02-24 11:09:38 +0300

    added kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,18 @@ from datetime import datetime
 
 class BaseModel:
     """Represents the base model class for the HBnB console"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """__init__ method called by every new instance"""
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == "created_at" or i == "updated_at":
+                    self.__dict__[i] = datetime.strptime(j, tform)
+                else:
+                    self.__dict__[i] = j
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
