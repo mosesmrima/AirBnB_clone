e2096e12b0d783041cb7cf3cb9724977d2884e7f
mosesmrima <mrimamss@gmail.com>
2021-02-26 10:25:45 +0300

    parser for the console arguments

---
+++ parser.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains the parser module"""
+import re
+from shlex import split
+def parse(arg):
+    """
+    This function parses the cmd arguments passed by the user to the console
+    :param arg: a string of passed arguments
+    :return: returns a list of arguments
+    """
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
