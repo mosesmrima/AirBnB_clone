439e67151e2c6f1202f2e61c57ebefce9c1df87e
mosesmrima <mrimamss@gmail.com>
2021-11-10 23:37:48 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,24 +4,35 @@
 
 import uuid
 from datetime import datetime
+import json
+import models
 
 
 class BaseModel:
     """this is the BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """init method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """update the updated_at attribute with current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def __str__(self):
         """ """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def to_dict(self):
         """return a dictionary containing __dict__ of an instance"""
