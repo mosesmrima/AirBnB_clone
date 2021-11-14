9d60898d3ec09c0edd8021003fb162a689ff90bf
mosesmrima <mrimamss@gmail.com>
2021-11-14 22:48:20 +0300

    added new classes

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ import cmd
 import sys
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models.__init__ import storage
 
 
@@ -14,7 +19,12 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb)"
     classes = {"BaseModel": "BaseModel",
-               "User": "User"
+               "User": "User",
+               "Place": "Place",
+               "State": "State",
+               "City": "City",
+               "Amenity": "Amenity"
+               "Review": "Review"
                }
 
     def do_quit(self, arg):
