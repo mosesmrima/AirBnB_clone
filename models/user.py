cbb7945316e2fd85e843a32b56affb4ad1b6bb34
mosesmrima <mrimamss@gmail.com>
2021-11-14 21:44:45 +0300

    user model

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" this module contains one class """
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User clas defination"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
